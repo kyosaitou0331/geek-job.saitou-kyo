900b68f650eecde4e46eb7c1d02426b6439d48d7
owner-PC\owner <today151533778800@gmail.com>
2018-05-24 13:45:01 +0900

    標準クラスの文字列編１の完了

---
+++ moziretu1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author owner
+ */
+public class moziretu1 {
+     public static void main(String[] args){
+        String name="齊藤京";
+        System.out.println("氏名の文字数は"+name.length()+"です。");
+        System.out.println("氏名のバイト数は"+name.getBytes().length+"です。");
+    }
+    
+}
