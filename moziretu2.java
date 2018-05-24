c3f987de16bdf707669b9cd6dbd36921d2419d74
owner-PC\owner <today151533778800@gmail.com>
2018-05-24 13:45:58 +0900

    標準クラスの文字列編２の完了

---
+++ moziretu2.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author owner
+ */
+public class moziretu2 {
+    public static void main(String[] args){
+        String adress="today151533778800@gmail.com";
+        System.out.println(adress.substring(17));
+        
+    }  
+}
